ad9a2b7a22bdfae209c18afc6096b4cef792bc71
Pranav489 <pranavsonawane489@gmail.com>
2025-04-30 10:05:44 +0530

    contact franchise factoryoutlet updated

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ShowroomGalleryController;
+use App\Http\Controllers\FranchiseInfoController;
+use App\Http\Controllers\FranchiseTestimonialController;
 use App\Http\Controllers\ContactInformationController;
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\BlogPostController;
 
+Route::get('/api/showroomgallery', [ShowroomGalleryController::class, 'index']);
 Route::get('/api/contact', [ContactInformationController::class, 'index']);
+Route::get('/api/franchise', [FranchiseInfoController::class, 'index']);
+Route::get('/api/testimonials', FranchiseTestimonialController::class);
+Route::get('/api/franchise/brochure', [FranchiseInfoController::class, 'downloadBrochure']);
+Route::get('/franchise-info/brochure/download', [FranchiseInfoController::class, 'serveBrochure']);
+
 
 Route::get('/api/blog', [BlogController::class, 'index']);
 Route::get('/api/blog/recent', [BlogController::class, 'recent']);
